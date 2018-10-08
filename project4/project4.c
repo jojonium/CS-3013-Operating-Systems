49d71954619b64a25f75e027a2886cabeb151636
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-10-08 09:46:23 -0400

    finished part 1

--- project4/project4.c
+++ project4/project4.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
+#include <sys/mman.h>
 
 #define DEFAULT_CHUNK_SIZE 1024
 
@@ -10,6 +12,7 @@ int main(int argc, char *argv[]) {
 	size_t chunkSize;
 	int infile; // input file descriptor
 	int charsRead, bufIndex, searchIndex, matches, fileSize;
+	struct stat sb;
 	char *buf;
 
 	// check arguments
@@ -20,9 +23,15 @@ int main(int argc, char *argv[]) {
 
 	// set chunk size
 	if (argc > 3) {
-		chunkSize = atoi(argv[3]);
+		if (strcmp(argv[3], "mmap") == 0) {
+			printf("Starting in memory mapping mode...\n");
+			chunkSize = 0;
+		} else {
+			printf("Starting in read mode...\n");
+			chunkSize = atoi(argv[3]);
+		}
 		if (chunkSize > 8192) { // enforce chunkSize limit
-			printf("Err: chunk size exceeded limit (8192 bytes). ");
+			printf("Chunk size exceeded limit (8192 bytes). ");
 			printf("Defaulting to %d bytes\n", DEFAULT_CHUNK_SIZE);
 			chunkSize = DEFAULT_CHUNK_SIZE;
 		}
@@ -42,10 +51,24 @@ int main(int argc, char *argv[]) {
 	matches = 0;
 	searchIndex = 0;
 	fileSize = 0;
-	while ((charsRead = read(infile, buf, chunkSize))) {
-		bufIndex = 0;
-		fileSize += charsRead;
-		while (bufIndex < charsRead) {
+	bufIndex = 0;
+	if (chunkSize == 0) {
+		// MMAP MODE
+
+		// stat file to obtain its size
+		if (fstat(infile, &sb) < 0) {
+			printf("Failed to stat file\n");
+			exit(1);
+		}
+
+		// map memory
+		if ((buf = (char *)mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED,
+						infile, 0)) == (char *) -1) {
+			printf("Failed to mmap file\n");
+			exit(1);
+		}
+
+		while (buf[bufIndex] != '\0') {
 			if (argv[2][searchIndex] == '\0') {
 				// matched to the end
 				matches++;
@@ -56,10 +79,42 @@ int main(int argc, char *argv[]) {
 			} else {
 				searchIndex = 0;
 			}
-
 			bufIndex++;
 		}
+
+		fileSize = sb.st_size;
+
+		// clean up
+		if (munmap(buf, sb.st_size) < 0) {
+			printf("Failed to unmap memory\n");
+			exit(1);
+		}
 	}
+	else {
+		// READ MODE
+		while ((charsRead = read(infile, buf, chunkSize))) {
+			fileSize += charsRead;
+			while (bufIndex < charsRead) {
+				if (argv[2][searchIndex] == '\0') {
+					// matched to the end
+					matches++;
+					searchIndex = 0;
+				} 
+				else if (buf[bufIndex] == argv[2][searchIndex]){
+					// one character matched
+					searchIndex++;
+				} 
+				else {
+					searchIndex = 0;
+				}
+
+				bufIndex++;
+			}
+			bufIndex = 0;
+		}
+	}
+
+	close(infile);
 
 	printf("File size: %d bytes.\n", fileSize);
 	printf("Occurrences of the string \"%s\": %d\n", argv[2], matches);
